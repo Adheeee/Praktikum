7c735579d8560014b4f286a514bfc7777026e3a1
Adheeee <147314638+Adheeee@users.noreply.github.com>
2023-11-20 22:26:04 +0800

    Add files via upload

---
+++ main.java
@@ -0,0 +1,24 @@
+package Overload;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class main {
+
+    public static void main(String[] args) {
+        Overload ov = new Overload();
+
+        ov.Bulan();
+        ov.RunProgram();
+        ov.RunProgram("winword");
+        ov.Tahun();
+        ov.animasi();
+        ov.animasi(0, 100);
+        ov.jamKeDetik(0);
+        ov.toRed("aku");
+        ov.write();
+        ov.write("sayang");
+
+    }
+
+}
